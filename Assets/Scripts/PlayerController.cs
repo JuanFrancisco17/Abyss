6e03693884596644c45b98919e4f194499aa42d3
Juan Francisco <sanchez.bordallo.juanfrancisco@gmail.com>
2021-11-14 19:55:05 +0100

    Arreglos en el salto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,70 +4,83 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //Variables componentes
-    private Rigidbody2D _rb2d;
-    private SpriteRenderer sprite;
-
-    //Variables movimiento
-
-    [SerializeField]
-    float speed;
-
-    //Variables salto
-    public bool jumpRequest;
-    [SerializeField]
-    float jumpForce;
-    [SerializeField]
-    float fallMultiplier, lowJumpMultiplier;
+    //Variables de componentes
+    Rigidbody2D rb2d;
+    //Variables de movimiento
+    public float speed;
+    //Variables de salto
+    private bool _isGround;
+    public float jumpForce;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    float coyoteTimer = 0;
+    public float maxCoyoteTime = 0.25f;
+    bool _isCoyoteTime = false;
+    private bool _jumpRequest = false;
 
     void Start()
     {
-        _rb2d = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Vertical"))
+
+        if (Input.GetButtonDown("Up") && _isGround == true)
         {
-            jumpRequest = true;
+            _jumpRequest = true;
+            _isGround = false;
+        }
+
+        //contador Coyote Time
+        if (_isCoyoteTime)
+        {
+            coyoteTimer += Time.deltaTime;
+        }
+
+        //Fin Coyote Time
+        if (coyoteTimer >= maxCoyoteTime)
+        {
+            _isCoyoteTime = false;
+            coyoteTimer = 0;
         }
     }
 
     void FixedUpdate()
     {
-        float axisH = Input.GetAxisRaw("Horizontal");
-        _rb2d.velocity = new Vector2(axisH * speed, _rb2d.velocity.y);
 
-        if (axisH > 0)
-        {
-            sprite.flipX = false;
-        }
-        else if (axisH < 0)
-        {
-            sprite.flipX = true;
-        }
+        float axisH = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(axisH * speed, 0) * Time.deltaTime;
 
         //salto basico
-        if (jumpRequest)
+        if (_jumpRequest)
         {
-            //rb2d.velocity += Vector2.up * jumpForce;
-            _rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jumpRequest = false;
+            _jumpRequest = false;
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
         //salto mejorado
-        if (_rb2d.velocity.y < 0)
+        if (rb2d.velocity.y < 0)
         {
-            _rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
-        else if (_rb2d.velocity.y > 0 && !Input.GetButton("Up"))
+        else if (rb2d.velocity.y > 0 && !Input.GetButton("Up"))
         {
-            _rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 
-    void OnCollisionEnter(Collision other) {
-        
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        _isCoyoteTime = true;
+
+    }
+
+    void OnCollisionStay2D(Collision2D other) 
+    {
+        if (other.collider.tag == "Ground")
+        {
+            _isGround = true;
+        }
     }
 }
