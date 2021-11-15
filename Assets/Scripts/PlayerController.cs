a88373cfce60d43e86824a04e98d7b860f3d9094
Juan Francisco <sanchez.bordallo.juanfrancisco@gmail.com>
2021-11-15 10:50:17 +0100

    Codigo comentado y puesto un mini escenario

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,35 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //Variables de componentes
+    //----VARIABLES DE COMPONENTES----
     Rigidbody2D rb2d;
-    //Variables de movimiento
-    public float speed;
-    //Variables de salto
-    private bool _isGround;
-    public float jumpForce;
+    
+    //----VARIABLES DE MOVIMIENTO----
+
+    //Velocidad del jugador
+    [SerializeField]
+    float speed;
+    
+    //----VARIABLES DE SALTO----
+    
+    //Variable que comprueba si el jugador está en el suelo
+    public bool _isGround;
+    //Fuerza del salto
+    [SerializeField]
+    float jumpForce;
+    //Fuerza que tiene el jugador al caer en el suelo
     public float fallMultiplier = 2.5f;
+    //Fuerza pequeña para saltar poco
     public float lowJumpMultiplier = 2f;
+    //Contador del Coyote Time
     float coyoteTimer = 0;
+    //Tiempo maximo del Coyote Time
     public float maxCoyoteTime = 0.25f;
-    bool _isCoyoteTime = false;
-    private bool _jumpRequest = false;
+    //Variable que comprueba si esta el jugador en el Coyote Time
+    private bool _isCoyoteTime = false;
+    //Variable para ver si el jugador quiere saltar
+    public bool _jumpRequest = false;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
-
     void Update()
     {
-
+        //Comprobamos si puede saltar
         if (Input.GetButtonDown("Up") && _isGround == true)
         {
             _jumpRequest = true;
             _isGround = false;
         }
 
-        //contador Coyote Time
+        //Contador Coyote Time
         if (_isCoyoteTime)
         {
             coyoteTimer += Time.deltaTime;
@@ -48,18 +62,18 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-
+        //Registramos los ejes horizontales y movemos al jugador
         float axisH = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(axisH * speed, 0) * Time.deltaTime;
 
-        //salto basico
-        if (_jumpRequest)
+        //Salto basico
+        if (_jumpRequest == true)
         {
-            _jumpRequest = false;
             rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            _jumpRequest = false;
         }
 
-        //salto mejorado
+        //Salto mejorado
         if (rb2d.velocity.y < 0)
         {
             rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
